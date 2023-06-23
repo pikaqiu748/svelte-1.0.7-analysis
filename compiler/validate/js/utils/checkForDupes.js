f817338d8b9499edd50c58bf3356a405ca9248b8
hectorhli <hectorhli@tencent.com>
2023-06-23 22:01:19 +0800

    添加注释

--- compiler/validate/js/utils/checkForDupes.js
+++ compiler/validate/js/utils/checkForDupes.js
@@ -1,4 +1,46 @@
 export default function checkForDupes ( validator, properties ) {
+	// sourcemap/script测试例子
+	// validator:{
+	// 	error: [Function: error],
+	// 	warn: [Function: warn],
+	// 	templateProperties: {},
+	// 	errors: [],
+	// 	warnings: [],
+	// 	defaultExport: Node {
+	// 	  type: 'ExportDefaultDeclaration',
+	// 	  start: 23,
+	// 	  end: 84,
+	// 	  declaration: Node {
+	// 		type: 'ObjectExpression',
+	// 		start: 38,
+	// 		end: 84,
+	// 		properties: [Array]
+	// 	  }
+	// 	}
+	//   } 
+	//properties: [
+	// 	Node {
+	// 	  type: 'Property',
+	// 	  start: 42,
+	// 	  end: 81,
+	// 	  method: true,
+	// 	  shorthand: false,
+	// 	  computed: false,
+	// 	  key: Node { type: 'Identifier', start: 42, end: 50, name: 'onrender' },
+	// 	  kind: 'init',
+	// 	  value: Node {
+	// 		type: 'FunctionExpression',
+	// 		start: 51,
+	// 		end: 81,
+	// 		id: null,
+	// 		generator: false,
+	// 		expression: false,
+	// 		async: false,
+	// 		params: [],
+	// 		body: [Node]
+	// 	  }
+	// 	}
+	//   ]
 	const seen = Object.create( null );
 
 	properties.forEach( prop => {
