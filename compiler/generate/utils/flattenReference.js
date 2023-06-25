3c100be36fe5758c33092c8cd9b17d3271853a5b
hectorhli <hectorhli@tencent.com>
2023-06-25 12:01:06 +0800

    添加flattenReference注释

--- compiler/generate/utils/flattenReference.js
+++ compiler/generate/utils/flattenReference.js
@@ -1,16 +1,25 @@
-export default function flatten ( node ) {
-	const parts = [];
-	while ( node.type === 'MemberExpression' ) {
-		if ( node.computed ) return null;
-		parts.unshift( node.property.name );
+export default function flatten(node) {
+  // binding-input-text-deep文件
+  // Node {
+  // 	type: 'MemberExpression',
+  // 	start: 42,
+  // 	end: 51,
+  // 	object: Node { type: 'Identifier', start: 42, end: 46, name: 'user' },
+  // 	property: Node { type: 'Identifier', start: 47, end: 51, name: 'name' },
+  // 	computed: false
+  //   }
+  const parts = []
+  while (node.type === 'MemberExpression') {
+    if (node.computed) return null
+    parts.unshift(node.property.name)
 
-		node = node.object;
-	}
+    node = node.object
+  }
 
-	if ( node.type !== 'Identifier' ) return null;
+  if (node.type !== 'Identifier') return null
 
-	const name = node.name;
-	parts.unshift( name );
-
-	return { name, keypath: parts.join( '.' ) };
+  const name = node.name
+  parts.unshift(name)
+  //  return  { name: 'user', keypath: 'user.name' }
+  return { name, keypath: parts.join('.') }
 }
